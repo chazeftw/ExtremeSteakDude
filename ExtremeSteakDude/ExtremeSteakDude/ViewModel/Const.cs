4cc17361020f3292540110b1b816cbfe3e882a92
Martin Nielsen <martinnielsen92@hotmail.com>
2015-10-20 11:46:50 +0200

    More constants

--- ExtremeSteakDude/ExtremeSteakDude/ViewModel/Const.cs
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/Const.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace ExtremeSteakDude.ViewModel
 {
-    class Const
+    static class Const
     {
-        private readonly int[] RESULUTION = { 1280, 720 };
-        private readonly int[] DEF_STARTPOS = { 100, 400 };
-
+        public static readonly int[] RESULUTION = { 1280, 720 };
+        public static readonly int[] DEF_STARTPOS = { 100, 400 };
+        public static readonly String[] MAP_PATH =
+            {
+            "C:\Users\Martin\Source\Repos\ExtremeSteakDude\ExtremeSteakDude\ExtremeSteakDude\Levels\level_1_1.bmp"
+            }
     }
 }
--- ExtremeSteakDude/ExtremeSteakDude/ViewModel/GameController.cs
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/GameController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExtremeSteakDude.
 
 namespace ExtremeSteakDude.ViewModel
 {
     class GameController
     {
-        public void initializeMap()
+        public void initializeMap(int mapIndex)
         {
 
         }
