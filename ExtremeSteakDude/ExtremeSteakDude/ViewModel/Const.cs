13f84cf494bd95e2cd3f5a87adcb12c2d01555b4
Martin Nielsen <martinnielsen92@hotmail.com>
2015-10-20 11:17:42 +0200

    Made test window and some files in vm

---
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeSteakDude.ViewModel
+{
+    class Const
+    {
+        private readonly int[] RESULUTION = { 1280, 720 };
+        private readonly int[] DEF_STARTPOS = { 100, 400 };
+
+    }
+}
