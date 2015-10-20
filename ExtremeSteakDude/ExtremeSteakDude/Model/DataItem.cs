ed5293cda49d5207d0acb6c5f378ed3188a12ce9
chazeftw <rasmusloje@hotmail.com>
2015-10-20 10:14:41 +0200

    This should work

---
+++ ExtremeSteakDude/ExtremeSteakDude/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtremeSteakDude.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
