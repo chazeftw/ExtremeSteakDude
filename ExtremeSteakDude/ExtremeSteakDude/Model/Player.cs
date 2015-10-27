73e39de699371c8eaecd8c6f6510283a3d733a14
Sverri Reinert <sverrireinert@live.com>
2015-10-27 12:00:41 +0100

    added movement methods

--- ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
+++ ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
@@ -11,11 +11,61 @@ namespace ExtremeSteakDude.Model
         // position
         public static int x { get; set; }
         public static int y { get; set; }
+
         // vectors
         public static int vx { get; set; }
         public static int vy { get; set; }
 
         public static bool inAir { get; set; }
- 
+        public static bool onWallRight { get; set; }
+        public static bool onWallLeft { get; set; }
+
+        public static void Jump()
+        {
+            if (inAir)
+            {
+                // do nothing
+            }
+            else if (onWallRight)
+            {
+                vx = -50;
+                vy = 50;
+                onWallRight = false;
+            }
+            else if (onWallLeft)
+            {
+                vx = 50;
+                vy = 50;
+                onWallLeft = false;
+            }
+            else
+            {
+                vy = 50;
+                inAir = true;
+            }
+                
+
+        }
+
+        public static void WallSlide(bool orientation)
+        {
+            if(orientation)
+            {
+                onWallRight = true;
+            }else
+            {
+                onWallLeft = true;
+            }
+            vx = 0;
+        }
+
+        public static void Land()
+        {
+            inAir = false;
+            onWallRight = false;
+            onWallLeft = false;
+            vy = 0;
+        }
+
     }
 }
