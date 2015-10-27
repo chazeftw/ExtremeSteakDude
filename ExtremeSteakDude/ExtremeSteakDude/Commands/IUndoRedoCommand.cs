2ba0ad93b5cfb3dee137a317c6a4d10b62ac47f6
Sverri Reinert <sverrireinert@live.com>
2015-10-27 10:13:37 +0100

    Implemented command classes

---
+++ ExtremeSteakDude/ExtremeSteakDude/Commands/IUndoRedoCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeSteakDude.Commands
+{
+    interface IUndoRedoCommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
