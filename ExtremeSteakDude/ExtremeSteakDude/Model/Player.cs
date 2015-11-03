fcf13b26fc54d46143558e3e00127383a7e6ecf8
Martin Nielsen <martinnielsen92@hotmail.com>
2015-11-03 11:30:41 +0100

    lavet en movement controller

--- ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
+++ ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
@@ -20,52 +20,7 @@ namespace ExtremeSteakDude.Model
         public static bool onWallRight { get; set; }
         public static bool onWallLeft { get; set; }
 
-        public static void Jump()
-        {
-            if (inAir)
-            {
-                // do nothing
-            }
-            else if (onWallRight)
-            {
-                vx = -50;
-                vy = 50;
-                onWallRight = false;
-            }
-            else if (onWallLeft)
-            {
-                vx = 50;
-                vy = 50;
-                onWallLeft = false;
-            }
-            else
-            {
-                vy = 50;
-                inAir = true;
-            }
-                
-
-        }
-
-        public static void WallSlide(bool orientation)
-        {
-            if(orientation)
-            {
-                onWallRight = true;
-            }else
-            {
-                onWallLeft = true;
-            }
-            vx = 0;
-        }
-
-        public static void Land()
-        {
-            inAir = false;
-            onWallRight = false;
-            onWallLeft = false;
-            vy = 0;
-        }
+        
 
     }
 }
---
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/MovementController.cs
@@ -0,0 +1,64 @@
+﻿using ExtremeSteakDude.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeSteakDude.ViewModel
+{
+    class MovementController
+    {
+        public MovementController()
+        {
+
+        }
+        public static void Jump()
+        {
+            if (Player.inAir)
+            {
+                // do nothing
+            }
+            else if (Player.onWallRight)
+            {
+                Player.vx = -50;
+                Player.vy = 50;
+                Player.onWallRight = false;
+            }
+            else if (Player.onWallLeft)
+            {
+                Player.vx = 50;
+                Player.vy = 50;
+                Player.onWallLeft = false;
+            }
+            else
+            {
+                Player.vy = 50;
+                Player.inAir = true;
+            }
+
+
+        }
+
+        public static void WallSlide(bool orientation)
+        {
+            if (orientation)
+            {
+                Player.onWallRight = true;
+            }
+            else
+            {
+                Player.onWallLeft = true;
+            }
+            Player.vx = 0;
+        }
+
+        public static void Land()
+        {
+            Player.inAir = false;
+            Player.onWallRight = false;
+            Player.onWallLeft = false;
+            Player.vy = 0;
+        }
+    }
+}
