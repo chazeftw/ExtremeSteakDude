30ec00a3ec3bd885b03c92792a1321c28537fe27
Sverri Reinert <sverrireinert@live.com>
2015-11-13 13:09:01 +0100

    d

--- ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
+++ ExtremeSteakDude/ExtremeSteakDude/Model/Player.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExtremeSteakDude.Model
 {
-    class Player
+    static class Player
     {
         // position
         public static int x { get; set; }
@@ -21,102 +20,10 @@ namespace ExtremeSteakDude.Model
         public static bool onWallRight { get; set; }
         public static bool onWallLeft { get; set; }
 
-        public bool moveRight = false;
-        public bool moveLeft = false;
-        public bool jump = false;
-        public Timer moveTimer;
-
-
-        public Player()
+        internal static void Jump()
         {
-            moveTimer = new Timer(x => Move(), null, 0, 50);
+            throw new NotImplementedException();
         }
-
-        private void Move()
-        {
-            x = x + vx;
-            y = y + vy;
-        }
-
-        public static void Jump()
-        {
-            if (inAir)
-            {
-                // do nothing
-            }
-            else if (onWallRight)
-            {
-                vx = -50;
-                vy = 50;
-                onWallRight = false;
-            }
-            else if (Player.onWallLeft)
-            {
-                vx = 50;
-                vy = 50;
-                onWallLeft = false;
-            }
-            else
-            {
-                vy = 50;
-                inAir = true;
-            }
-
-
-        }
-
-        public void MoveRight()
-        {
-            if (vx < 50)
-            {
-                if (50 - vx <= 10)
-                {
-                    vx = vx + 10;
-                }
-                else
-                {
-                    vx = 50;
-                }
-            }
-        }
-
-        public void MoveLeft()
-        {
-            if (vx > -50)
-            {
-                if (-50 - vx >= -10)
-                {
-                    vx = vx - 10;
-                }
-                else
-                {
-                    vx = -50;
-                }
-            }
-        }
-
-
-        public static void WallSlide(bool orientation)
-        {
-            if (orientation)
-            {
-                onWallRight = true;
-            }
-            else
-            {
-                onWallLeft = true;
-            }
-            vx = 0;
-        }
-
-        public static void Land()
-        {
-            inAir = false;
-            onWallRight = false;
-            onWallLeft = false;
-            vy = 0;
-        }
-
 
     }
 }
