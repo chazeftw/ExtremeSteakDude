f4f6d335ce3ac3ae618d3d8c92186b9651be9a81
Sverri Reinert <sverrireinert@live.com>
2015-11-13 12:52:16 +0100

    added new command

---
+++ ExtremeSteakDude/ExtremeSteakDude/Commands/MomentumCommand.cs
@@ -0,0 +1,32 @@
+﻿using ExtremeSteakDude.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtremeSteakDude.Commands
+{
+    class MomentumCommand : IUndoRedoCommand
+    {
+        int x; int y;
+
+        public MomentumCommand(int x,int y)
+        {
+            this.y = y;
+            this.x = x;
+        }
+
+        public void Execute()
+        {
+            Player.x = Player.x + x;
+            Player.y = Player.y + y;
+        }
+
+        public void Undo()
+        {
+            Player.x = Player.x - x;
+            Player.y = Player.y - y;
+        }
+    }
+}
