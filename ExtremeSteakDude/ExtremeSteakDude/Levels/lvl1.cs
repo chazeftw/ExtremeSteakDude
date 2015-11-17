41833b011ac8160131be104b31e6cd1f265b915c
Martin Nielsen <martinnielsen92@hotmail.com>
2015-11-17 09:45:05 +0100

    flyttet Const til mappen Constants

--- ExtremeSteakDude/ExtremeSteakDude/Levels/lvl1.cs
+++ ExtremeSteakDude/ExtremeSteakDude/Levels/lvl1.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using ExtremeSteakDude.ViewModel;
 
 namespace ExtremeSteakDude.Levels
 {
-    class lvl1
+    public static class lvl1
     {
+        readonly static int startX;
+        readonly static int startY;
+        static List<Rectangle> mapObjects = new List<Rectangle>(
+            new Rectangle(new Point(1,1),new Size(10))
+            );
+
+        
 
     }
 }
