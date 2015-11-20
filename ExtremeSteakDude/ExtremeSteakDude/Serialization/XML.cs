6f0011dba41546cb3bf71adefd5a29677277d801
kangarooooooo <eirikonielsen@gmail.com>
2015-11-20 14:02:23 +0100

    asdf

---
+++ ExtremeSteakDude/ExtremeSteakDude/Serialization/XML.cs
@@ -0,0 +1,42 @@
+﻿using ExtremeSteakDude.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ExtremeSteakDude.Serialization
+{
+    class XML
+    {
+     private XmlSerializer ser = new XmlSerializer(typeof(HighScores));
+
+
+     public HighScores HighScores {
+         get { return Deserialize(); }
+         set { Serialize(value); }
+     }
+     private void Serialize(HighScores value)
+     {
+         TextWriter writer = new StreamWriter("Highscores.xml");
+         ser.Serialize(writer, value);
+         writer.Close();
+     }
+
+     private HighScores Deserialize()
+     {
+         // Construct an instance of the XmlSerializer with the type
+         // of object that is being deserialized.
+         XmlSerializer mySerializer =
+         new XmlSerializer(typeof(HighScores));
+         // To read the file, create a FileStream.
+         FileStream myFileStream =
+         new FileStream("Highscores.xml", FileMode.Open);
+         // Call the Deserialize method and cast to the object type.
+         return (HighScores)mySerializer.Deserialize(myFileStream);
+     }
+     
+    }
+}
