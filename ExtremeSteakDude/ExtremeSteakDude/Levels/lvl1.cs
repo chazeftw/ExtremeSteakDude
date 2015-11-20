a0403d684105fe63e94cdb83e9a4634eab540682
Martin Nielsen <martinnielsen92@hotmail.com>
2015-11-20 14:02:18 +0100

    opdateret størrelseskonstanter

--- ExtremeSteakDude/ExtremeSteakDude/Levels/lvl1.cs
+++ ExtremeSteakDude/ExtremeSteakDude/Levels/lvl1.cs
@@ -18,9 +18,9 @@ namespace ExtremeSteakDude.Levels
         {
             this.startX = 50;
             this.startY = 200;
-            Rectangle ground = new Rectangle(0, Const.HEIGHT - 100, Const.WIDTH, 100);
+            Rectangle ground = new Rectangle(0, Const.MAPHEIGHT - 100, Const.MAPWIDTH, 100);
             mapObjects.Add(ground);
-            Rectangle obstacle1 = new Rectangle(Const.WIDTH / 2, Const.HEIGHT - 150, 25, 50);
+            Rectangle obstacle1 = new Rectangle(Const.MAPWIDTH / 2, Const.MAPHEIGHT - 150, 25, 50);
             mapObjects.Add(obstacle1);
             
         }
