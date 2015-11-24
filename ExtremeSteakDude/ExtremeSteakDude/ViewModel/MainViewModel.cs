14249cea2180e6c32826becd2bd3f70c2ef3057d
chazeftw <rasmusloje@hotmail.com>
2015-11-24 14:36:37 +0100

    So much shit

--- ExtremeSteakDude/ExtremeSteakDude/ViewModel/MainViewModel.cs
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/MainViewModel.cs
@@ -90,7 +90,8 @@ namespace ExtremeSteakDude.ViewModel
                 case Key.Space:
                     mc.jump = true;
                     // For jump animation
-                    BitmapImage bm = new BitmapImage(new Uri("/ExtremeSteakDude;component/Levels/meatboyjump.jpg", UriKind.RelativeOrAbsolute));
+                    BitmapImage bm = new BitmapImage(new Uri(player.MeatboyImageJump, UriKind.RelativeOrAbsolute));
+                    player.meatboyImage = player.MeatboyImageJump;
                     break;
                 case Key.Z:
                     mc.isUndoMode = !mc.isUndoMode;
@@ -111,8 +112,8 @@ namespace ExtremeSteakDude.ViewModel
                 case Key.Space:
                     mc.jump = false;
                     // For jump animation
-                    BitmapImage bm = new BitmapImage(new Uri("/ExtremeSteakDude;component/Levels/meatboy.jpg", UriKind.RelativeOrAbsolute));
-                   // player.Source = bm;
+                    BitmapImage bm = new BitmapImage(new Uri(player.MeatboyImageJump, UriKind.RelativeOrAbsolute));
+                    player.meatboyImage = player.MeatboyImage;
                     break;
             }
         }
