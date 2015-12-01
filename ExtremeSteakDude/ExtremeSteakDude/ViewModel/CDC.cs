e7d871cfd91fa09181ec64a2d4291628e284e391
Sverri Reinert <sverrireinert@live.com>
2015-12-01 09:31:57 +0100

    cccc

--- ExtremeSteakDude/ExtremeSteakDude/ViewModel/CDC.cs
+++ ExtremeSteakDude/ExtremeSteakDude/ViewModel/CDC.cs
@@ -47,10 +47,10 @@ namespace ExtremeSteakDude.ViewModel
             rightSideBot[0] = mc.p[0].x + 32 + mc.p[0].vx;
             rightSideBot[1] = mc.p[0].y + 30 + mc.p[0].vy;
 
-            rightTop[0] = mc.p.x + mc.p.vx;
-            rightTop[1] = mc.p.y - 1 + mc.p.vy;
-            leftTop[0] = mc.p.x + 32 + mc.p.vx;
-            leftTop[1] = mc.p.y - 1 + mc.p.vy;
+            rightTop[0] = mc.p[0].x + mc.p[0].vx;
+            rightTop[1] = mc.p[0].y - 1 + mc.p[0].vy;
+            leftTop[0] = mc.p[0].x + 32 + mc.p[0].vx;
+            leftTop[1] = mc.p[0].y - 1 + mc.p[0].vy;
 
 
             if (image.GetPixel(rightFoot[0], rightFoot[1]) != backGround || image.GetPixel(leftFoot[0], leftFoot[1]) != backGround)
@@ -76,10 +76,10 @@ namespace ExtremeSteakDude.ViewModel
             }
             if(image.GetPixel(leftTop[0],leftTop[1]) != backGround || image.GetPixel(rightTop[0],rightTop[1]) != backGround)
             {
-                mc.p.top = true;
+                mc.p[0].top = true;
             }else
             {
-                mc.p.top = false;
+                mc.p[0].top = false;
             }
 
             
@@ -92,10 +92,10 @@ namespace ExtremeSteakDude.ViewModel
                     rightSideBot[1]--;
                     i++;
                 }
-                mc.p.vy = mc.p.vy - i;
-            } else if (mc.p.top && !mc.p.inAir)
+                mc.p[0].vy = mc.p[0].vy - i;
+            } else if (mc.p[0].top && !mc.p[0].inAir)
             {
-                if (mc.p.onWallLeft)
+                if (mc.p[0].onWallLeft)
                 {
                     int i = 0;
                     while (image.GetPixel(leftTop[0], leftTop[1]) != backGround && image.GetPixel(leftFoot[0], leftFoot[1]) != backGround)
@@ -104,18 +104,18 @@ namespace ExtremeSteakDude.ViewModel
                         leftFoot[0]++;
                         i++;
                     }
-                    mc.p.vx = mc.p.vx + i;
+                    mc.p[0].vx = mc.p[0].vx + i;
                 }
-                else if (mc.p.onWallRight)
+                else if (mc.p[0].onWallRight)
                 {
                     int i = 0;
                     while (image.GetPixel(leftTop[0], leftTop[1]) != backGround && image.GetPixel(leftFoot[0], leftFoot[1]) != backGround)
                     {
                         rightTop[0]--;
                         rightFoot[0]--;
-                        i--;
+                        i++;
                     }
-                    mc.p.vx = mc.p.vx - i;
+                    mc.p[0].vx = mc.p[0].vx - i;
             }
             
             } else if (image.GetPixel(leftSideTop[0], leftSideTop[1]) != backGround || image.GetPixel(rightSideTop[0], rightSideTop[1]) != backGround)
@@ -127,7 +127,7 @@ namespace ExtremeSteakDude.ViewModel
                     rightSideTop[1]++;
                     i++;
                 }
-                mc.p.vy = mc.p.vy + i;
+                mc.p[0].vy = mc.p[0].vy + i;
             }
         }
     }
